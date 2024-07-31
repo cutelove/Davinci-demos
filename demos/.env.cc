73d9b5e262b4b1ff0808b5d88bb268a0b69f15f8
cutelove <qtpig@qq.com>
2024-07-31 22:03:06 +0800

    add example for pdf table process

--- demos/.env.cc
+++ demos/.env.cc
@@ -1,2 +1,12 @@
-OPENAI_API_KEY="sk-4ZzoWljxLKuqiF5FYqUz83x5GrINPvIqo9a5zUulNIBaTdtM"
-OPENAI_BASE_URL="https://api.agicto.cn/v1"
+OPENAI_API_KEY="sk-Qj66hJ3sucWiqOxpQFGFcEB5ymrsdKY91SXsWshw1kSVfd3y"
+OPENAI_BASE_URL="https://api.fe8.cn/v1"
+SERPAPI_API_KEY="2e9a661f6f4583b6882994a915ad7a643f7730ec56ed799096957f803efe862a"
+GOOGLE_API_KEY="xxx"
+
+MOONSHOT_API_KEY="sk-42geL1n7lxativJaDajoruDXC7D1QdjerJhooeoi0xDWkeUO"
+MOONSHOT_API_BASE="https://api.moonshot.cn/v1"
+
+LANGFUSE_SECRET_KEY="sk-lf-1579bf78-576d-4fbe-b4ed-bab9817481f4"
+LANGFUSE_PUBLIC_KEY="pk-lf-2ba260b6-9240-459b-8bf5-d1c45be7f3c1"
+LANGFUSE_HOST="https://us.cloud.langfuse.com"
+
